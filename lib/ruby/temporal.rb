45f18f59b94bcf898937c01d32ae63df4a5fc095
Leonardo Prado <leoprado@paypal.com>
2025-12-04 21:04:07 -0300

    bundle gem new

---
+++ lib/ruby/temporal.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "temporal/version"
+
+module Ruby
+  module Temporal
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
