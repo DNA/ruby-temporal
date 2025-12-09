cfca15fd76ff20c227b4598db9aedd3e3e02afcd
Leonardo Prado <leoprado@paypal.com>
2025-12-08 23:54:32 -0300

    Add PlainDate basic constructor test

---
+++ test/test262/plain_date/test_constructor.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "temporal/plain_date"
+
+require "test_helper"
+
+# Basic tests for the PlainDate constructor
+#
+# @note test262 info
+#   original: test/test262/built-in/PlainDate/constructor.js
+#   esid: sec-temporal.plaindate
+#   description: Temporal.PlainDate constructor cannot be called as a function
+#   info: |
+#       1. If NewTarget is undefined, throw a TypeError exception.
+#   features: [Temporal]
+class Temporal::PlainDate::Constructor < Minitest::Test
+  def test_raise_on_direct_call
+    assert_raises(NoMethodError) { Temporal::PlainDate(2021, 12, 22, :iso8601) }
+  end
+end
