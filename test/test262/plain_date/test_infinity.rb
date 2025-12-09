8ed757357a05f1645429367f9b0fbd59e14193f1
Leonardo Prado <leoprado@paypal.com>
2025-12-08 23:58:46 -0300

    Add tests for negative infinity

--- test/test262/plain_date/test_infinity.rb
+++ test/test262/plain_date/test_infinity.rb
@@ -7,19 +7,23 @@ require "test_helper"
 # PlainDate throws a RangeError if any value is Infinity
 #
 # @note test262 info
-#   original: test/test262/built-in/PlainDate/infinity-throws-rangeerror.js
+#   original:
+#     - test/test262/built-in/PlainDate/infinity-throws-rangeerror.js
+#     - test/test262/built-in/PlainDate/negative-infinity-throws-rangeerror.js
 #   description: Temporal.PlainDate throws a RangeError if any value is Infinity
 #   esid: sec-temporal.plaindate
 #   includes: [compareArray.js, temporalHelpers.js]
 #   features: [Temporal]
 class Temporal::PlainDate::Infinity < Minitest::Test
   {
-
-    year: [Float::INFINITY, 1, 1],
-    month: [1970, Float::INFINITY, 1],
-    day: [1970, 1, Float::INFINITY],
+    positive_infinity_on_year: [Float::INFINITY, 1, 1],
+    positive_infinity_on_month: [1970, Float::INFINITY, 1],
+    positive_infinity_on_day: [1970, 1, Float::INFINITY],
+    negative_infinity_on_year: [-Float::INFINITY, 1, 1],
+    negative_infinity_on_month: [1970, -Float::INFINITY, 1],
+    negative_infinity_on_day: [1970, 1, -Float::INFINITY],
   }.each do |name, data|
-    define_method("test_raise_on_in_infinity_#{name}") do
+    define_method("test_raise_on_#{name}") do
       year, month, day = data
 
       assert_raises(RangeError) { Temporal::PlainDate.new(year, month, day) }
