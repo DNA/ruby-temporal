1d625badbaa5eb881b7f6cebbc01ab7229c92291
Leonardo Prado <leonardoprado@gmail.com>
2025-12-28 23:24:25 -0300

    Sort imports alphabetically

--- lib/temporal.rb
+++ lib/temporal.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
-require_relative "temporal/version"
 require_relative "temporal/duration"
 require_relative "temporal/plain_date"
 require_relative "temporal/plain_date_time"
 require_relative "temporal/plain_month_day"
-require_relative "temporal/plain_year_month"
 require_relative "temporal/plain_time"
+require_relative "temporal/plain_year_month"
 require_relative "temporal/units/base"
 require_relative "temporal/units/day"
 require_relative "temporal/units/month"
 require_relative "temporal/units/year"
+require_relative "temporal/version"
 require_relative "temporal/zoned_date_time"
 
 module Temporal
